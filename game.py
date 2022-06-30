b9ddc0d2dc0b644d44a9068507bb1c8a988a890c
Rino <esmerinoneto1996@gmail.com>
2022-06-29 21:33:45 -0400

    Fixed issue

--- game.py
+++ game.py
@@ -14,12 +14,10 @@ def menu():
     draws = 0
     losses = 0
     matches = 0
-    enemy = randint(1, 3)
-    enex = str(enemy)
 
     while user_input.upper() != "Q":
         if user_input.upper() == 'A':
-            wins, draws, losses, matches,  = game(wins, draws, losses, matches, enex)
+            wins, draws, losses, matches = game(wins, draws, losses, matches)
         elif user_input.upper() == 'B':
             record(wins, losses, draws, matches)
         user_input = input(menu_prompt)
@@ -27,38 +25,49 @@ def menu():
 
 
 
-def game(wins, draws, losses, matches, enex):
+def game(wins, draws, losses, matches):
 
     user_input = int(input("\nROCK = 1 \nPAPER = 2\nSCISSOR = 3\nQUIT = 0\n\nROCK, PAPER OR SCISSORS: "))
+    enemy = randint(1, 3)
+    enex = str(enemy)
     for rival in enex:
+        int_rival = int(rival)
         while user_input > 0 and user_input < 4:
-            try:
-                matches += 1
 
-                if rival == "2":
-                    losses += 1
-                    print(f"YOU LOST")
+            matches += 1
 
-                elif rival == "3":
-                    wins += 1
-                    print(f"YOU WON")
+            if int_rival == user_input:
+                draws +=1
+                print(f"YOU DRAWN")
 
-                elif rival == "1":
-                    draws +=1
-                    print(f"YOU DRAWN")
+            elif int_rival == 2 and user_input != 3:
+                losses += 1
+                print(f"YOU LOST")
 
-            except:
-                ValueError(f" ONLY NUMBERS BETWEEN 1-3")
-                continue
+            elif int_rival == 3 and user_input != 1:
+                losses += 1
+                print(f"YOU LOST")
+
+            elif int_rival == 1 and user_input != 2:
+                losses += 1
+                print(f"YOU LOST")
+
+            else:
+                wins += 1
+                print(f"YOU WON")
+
 
 
 
             return wins, draws, losses, matches
+
+
 def record(wins, losses, draws, matches):
+
     print(f"YOUR RECORD\nWINS: {wins}\nLOSSES: {losses}\nDRAWS: {draws}\nTOTAL MATCHES: {matches}")
 
-menu()
 
+menu()
 
 
 
