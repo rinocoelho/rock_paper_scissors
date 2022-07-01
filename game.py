56387cfbcb16f2bf88a538c6d5a73136ef4109b3
Rino <esmerinoneto1996@gmail.com>
2022-06-30 22:49:26 -0400

    ask input fix

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from random import *
+import os
 
 menu_prompt = (
     '- Press A to PLAY\n'
@@ -9,62 +10,82 @@ menu_prompt = (
 
 
 def menu():
-    user_input = ""
+    menu_input = ""
     wins = 0
     draws = 0
     losses = 0
     matches = 0
 
-    while user_input.upper() != "Q":
-        if user_input.upper() == 'A':
+    while menu_input.upper() != "Q":
+        if menu_input.upper() == 'A':
             wins, draws, losses, matches = game(wins, draws, losses, matches)
-        elif user_input.upper() == 'B':
+        elif menu_input.upper() == 'B':
             record(wins, losses, draws, matches)
-        user_input = input(menu_prompt)
-
-
+        menu_input = input(menu_prompt)
 
 
 def game(wins, draws, losses, matches):
 
-    user_input = int(input("\nROCK = 1 \nPAPER = 2\nSCISSOR = 3\nQUIT = 0\n\nROCK, PAPER OR SCISSORS: "))
     enemy = randint(1, 3)
     enex = str(enemy)
     for rival in enex:
         int_rival = int(rival)
-        while user_input > 0 and user_input < 4:
+        user_input = ask_input()
+        #while user_input > 0 and user_input < 4:
+            #user_input = int(input("\n ROCK = 1 \n PAPER = 2\n SCISSOR = 3\n\n ROCK, PAPER OR SCISSORS: "))
 
-            matches += 1
+        matches += 1
 
-            if int_rival == user_input:
-                draws +=1
-                print(f"YOU DRAWN")
+        if int_rival == user_input:
+            draws +=1
+            print(f"YOU DRAWN")
 
-            elif int_rival == 2 and user_input != 3:
-                losses += 1
-                print(f"YOU LOST")
+        elif int_rival == 2 and user_input != 3:
+            losses += 1
+            print(f"YOU LOST")
 
-            elif int_rival == 3 and user_input != 1:
-                losses += 1
-                print(f"YOU LOST")
+        elif int_rival == 3 and user_input != 1:
+            losses += 1
+            print(f"YOU LOST")
 
-            elif int_rival == 1 and user_input != 2:
-                losses += 1
-                print(f"YOU LOST")
+        elif int_rival == 1 and user_input != 2:
+            losses += 1
+            print(f"YOU LOST")
 
-            else:
-                wins += 1
-                print(f"YOU WON")
-
-
+        else:
+            wins += 1
+            print(f"YOU WON")
 
 
-            return wins, draws, losses, matches
+        #user_input = ask_input()
+
+        return wins, draws, losses, matches
+
+
+
 
 
 def record(wins, losses, draws, matches):
 
     print(f"YOUR RECORD\nWINS: {wins}\nLOSSES: {losses}\nDRAWS: {draws}\nTOTAL MATCHES: {matches}")
+
+def ask_input():
+
+    user_input = 0
+    while user_input < 1 or user_input > 3:
+        try:
+            user_input = int(input("\nROCK = 1\nPAPER = 2\nSCISSOR = 3\n\nROCK, PAPER OR SCISSORS: "))
+        except:
+            ValueError
+            print("Invalid input! Only numbers from 1 to 3")
+    return user_input
+
+
+
+
+
+
+
 
 
 menu()
